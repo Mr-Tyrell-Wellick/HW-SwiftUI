38b5ddb0714e0dc38ef45928959f8daa54573b76
Mr-Tyrell-Wellick <rorshix97@gmail.com>
2023-05-10 16:45:05 +0400

    Initial Commit

---
+++ HW-SwiftUI/HW_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_SwiftUIApp.swift
+//  HW-SwiftUI
+//
+//  Created by Ульви Пашаев on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
